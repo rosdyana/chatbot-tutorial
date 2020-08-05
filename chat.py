b9adab38ae27e82bcd748e094a68dccb3664bfd5
rosdyana <rosdyana.kusuma@gmail.com>
2020-08-06 01:28:06 +0800

    init chatbot

---
+++ chat.py
@@ -0,0 +1,23 @@
+from train import train, bag_of_words
+import numpy
+import random
+
+def chat(model, labels, words, data):
+    print("Start talking with the bot (type quit to stop)!")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+
+        results = model.predict([bag_of_words(inp, words)])
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+
+        print(random.choice(responses))
+
+model, labels, words, data = train(False)
+chat(model, labels, words, data)
